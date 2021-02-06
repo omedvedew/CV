9d5906090dbaecefc97c247915fa52103ab1349d
olexii <omedvedew@ukr.net>
2021-02-07 00:14:32 +0200

    added greetings modal

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // sidebar-script
 let sidebarArrow = document.querySelector(".sidebar-arrow");
 let mobileSidebar = document.querySelector(".mobile-sidebar");
+let mainContainer = document.querySelector(".main-container");
 
 function toggleSidebar () {
     mobileSidebar.classList.toggle("mobile-sidebar-active");
     sidebarArrow.classList.toggle("to-left-arrow");
+    mainContainer.classList.toggle("main-container-hidden");
 };
 
 sidebarArrow.addEventListener("click", toggleSidebar);
+
+// greetings-script
+let greetingsModal = document.querySelector(".greetings-modal");
+
+function hideGreetings () {
+    greetingsModal.classList.add("greetings-modal-hidden");
+};
+
+window.setTimeout(hideGreetings, 2000);
